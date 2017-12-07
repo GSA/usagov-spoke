cbfa3f6d05f6027515aa40acc7dcb82d28c7b217
achuluunkhuu <achuluunkhuu@ctacorp.com>
2017-12-07 14:27:13 -0500

    fix for https://ctac.myjetbrains.com/youtrack/issue/usagov-102093

--- sites/all/themes/usa/templates/taxonomy-term.tpl.php
+++ sites/all/themes/usa/templates/taxonomy-term.tpl.php
@@ -166,6 +166,7 @@ if (isset($term->field_type_of_page_to_generate['und'][0]['value']) && $term->fi
 
             <?php
                 print _print_social_media();
+                print '<p class="volver clearfix"><a href="#skiptarget"><span class="icon-backtotop-dwnlvl">'.t('Back to Top').'</span></a></p>';
                 print do_you_need_help();
                 print survey_on_pages();
             ?>
@@ -174,7 +175,6 @@ if (isset($term->field_type_of_page_to_generate['und'][0]['value']) && $term->fi
 
     </div>
 
-    <p class="volver clearfix"><a href="#skiptarget"><span class="icon-backtotop-dwnlvl"><?php print t('Back to Top'); ?></span></a></p>
 <?php endif; ?>
 
 
